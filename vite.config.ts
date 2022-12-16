eafefb2b11df14554e29ce67feb67433897dff6a
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-16 13:00:03 +0800

    chore(dx): use path aliasing
    makes importing of modules convenient

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import path from "path"
+
+import { defineConfig } from "vite"
+
+const ROOT_DIR = __dirname
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	"plugins": [vue()],
+	"resolve": {
+		"alias": {
+			"@": path.resolve(ROOT_DIR, "./src/components")
+		}
+	},
+	"server": {
+		"open": true
+	}
 })
