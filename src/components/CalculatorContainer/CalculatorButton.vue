13e91108eff819d2e5097721611faf1b7fdf5879
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-16 23:46:44 +0800

    feat(cmpnt): emit button value to screen
    at the time this was committed, screen is yet to be implemented

--- src/components/CalculatorContainer/CalculatorButton.vue
+++ src/components/CalculatorContainer/CalculatorButton.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+interface Props {
+	value: number
+}
+const props = defineProps<Props>()
+
+interface CustomEvents {
+	(event: "appendToScreen", valueToAppend: number): void
+}
+const emit = defineEmits<CustomEvents>()
+
+function appendToScreen() {
+	emit("appendToScreen", props.value)
+}
 </script>
 
 <template>
-	button
+	<button class="calculator-button" @click="appendToScreen">
+		{{ props.value }}
+	</button>
 </template>
 
 <style scoped lang="scss">
