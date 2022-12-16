77f83651b4710923b4b1f97c40c02d8a246884f8
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-16 20:05:37 +0800

    chore(config): reogranize imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 /// <reference types="vitest/config" />
-
-import vue from "@vitejs/plugin-vue"
 import path from "path"
-
 import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 const ROOT_DIR = __dirname
 
