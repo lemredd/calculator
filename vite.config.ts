6bc9ace5048aa9ed537b81dd42303888888b06b1
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-16 18:49:43 +0800

    chore(config): add `vitest` configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import vue from "@vitejs/plugin-vue"
 import path from "path"
 
@@ -6,6 +8,7 @@ import { defineConfig } from "vite"
 const ROOT_DIR = __dirname
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
 	"plugins": [vue()],
 	"resolve": {
@@ -15,5 +18,10 @@ export default defineConfig({
 	},
 	"server": {
 		"open": true
+	},
+	"test": {
+		"include": [
+			"**/*.{test,spec}.{ts,js}"
+		]
 	}
 })
