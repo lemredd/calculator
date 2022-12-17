c28008ebd3c877ab17be1f9487b2776521e4a4aa
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-17 11:52:16 +0800

    feat(intg): adhere integration based on test
    this may change on maturity

--- src/components/CalculatorContainer.vue
+++ src/components/CalculatorContainer.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { ref } from "vue"
+
 import Button from "@/CalculatorContainer/CalculatorButton.vue"
+import Screen from "@/CalculatorContainer/CalculatorScreen.vue"
+
+const valueToDisplay = ref("0")
 </script>
 
 <template>
-	hello
+	<Screen :value-to-display="valueToDisplay" />
+	<Button :value="1" />
 </template>
 
 <style scoped lang="scss">
