98325a6db961dbf5d5256c536ab0b297f43ba566
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-17 11:49:04 +0800

    test(intg): ensure  `CalculatorContainer`
    this tests the changing of screen value when a button is pressed

--- src/components/CalculatorContainer.test.ts
+++ src/components/CalculatorContainer.test.ts
@@ -1,7 +1,14 @@
+import { mount } from "@vue/test-utils"
+
 import Component from "./CalculatorContainer.vue"
 
 describe("Component: CalculatorContainer", () => {
-	it("can", () => {
-		console.log("it can")
+	it("can change screen value once a button is pressed", async() => {
+		const wrapper = mount(Component)
+		const calculatorBtn = wrapper.find(".calculator-button")
+		const calculatorScreen = wrapper.find(".calculator-screen").element as HTMLInputElement
+
+		await calculatorBtn.trigger("click")
+		expect(calculatorScreen.value).not.toEqual("0")
 	})
 })
