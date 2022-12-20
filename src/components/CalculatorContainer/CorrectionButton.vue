9deae04c13971681fab9c0ef2885d6529add54db
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-20 15:58:29 +0800

    feat(component): make `CorrectionButton`
    this will also mutate another separate screen.
    The separate screen will be developed soon.
    
    fix lemredd/calculator#32

---
+++ src/components/CalculatorContainer/CorrectionButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type Corrections = "CE"|"C"|""
+interface Props {
+	value: Corrections
+}
+const props = defineProps<Props>()
+
+interface CustomEvents {
+	(event: "appendToScreen", valueToAppend: string): void
+}
+const emit = defineEmits<CustomEvents>()
+
+function appendToScreen() {
+	emit("appendToScreen", props.value)
+}
+</script>
+
+<template>
+	<button class="correction-button" @click="appendToScreen">
+		{{ props.value }}
+	</button>
+</template>
+
+<style scoped lang="scss">
+</style>
