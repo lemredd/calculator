904d91e692d0cb59799d05cd99d6eeaa1d8d8704
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-20 20:29:45 +0800

    chore(intg): prepare using of evaluation helper

--- src/components/CalculatorContainer.vue
+++ src/components/CalculatorContainer.vue
@@ -3,6 +3,8 @@ import { computed, ref } from "vue"
 
 import type { PossibleButtonValues } from "@/types/buttons"
 
+import evaluate from "@/CalculatorContainer/helpers/evaluate"
+
 import Screen from "@/CalculatorContainer/CalculatorScreen.vue"
 import DigitalButton from "@/CalculatorContainer/DigitalButton.vue"
 import EvaluationButton from "@/CalculatorContainer/EvaluationButton.vue"
@@ -10,9 +12,15 @@ import OperationalButton from "@/CalculatorContainer/OperationalButton.vue"
 
 const valueToDisplay = ref("0")
 const isDisplayEmpty = computed(() => valueToDisplay.value === "0")
+
 function appendToScreen(valueToAppend: PossibleButtonValues) {
 	if (isDisplayEmpty.value) valueToDisplay.value = String(valueToAppend)
 	else valueToDisplay.value += ` ${valueToAppend}`
+}
+
+function evaluateExpression(valueToAppend: PossibleButtonValues) {
+	appendToScreen(valueToAppend)
+	return
 }
 </script>
 
@@ -20,7 +28,7 @@ function appendToScreen(valueToAppend: PossibleButtonValues) {
 	<Screen :value-to-display="valueToDisplay" />
 	<DigitalButton :value="1" @append-to-screen="appendToScreen" />
 	<OperationalButton value="+" @append-to-screen="appendToScreen" />
-	<EvaluationButton value="=" @append-to-screen="appendToScreen" />
+	<EvaluationButton value="=" @append-to-screen="evaluateExpression" />
 </template>
 
 <style scoped lang="scss">
