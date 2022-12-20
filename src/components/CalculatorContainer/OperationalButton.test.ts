4d27b23a4c4079fce52b92f7d07a06eb78c77c26
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-20 14:47:09 +0800

    test(component): prepare unit testing
    `OperationalButtons` will mutate separate screen.
    The separate screen will be developed soon.

---
+++ src/components/CalculatorContainer/OperationalButton.test.ts
@@ -0,0 +1,20 @@
+import { shallowMount } from "@vue/test-utils"
+
+import Component from "./OperationalButton.vue"
+
+describe("Component: CalculatorContainer/OperationalButton", () => {
+	it("can emit custom value", async() => {
+		const wrapper = shallowMount(Component, {
+			"props": {
+				"value": 1
+			}
+		})
+		const operationalBtn = wrapper.find("button.operational-button")
+		const operationalBtnValue = wrapper.props("value")
+		expect(operationalBtn.text()).toEqual(String(operationalBtnValue))
+
+		await operationalBtn.trigger("click")
+		const expectedEmission = wrapper.emitted("appendToScreen")
+		expect(expectedEmission).toHaveProperty("0.0", operationalBtnValue)
+	})
+})
