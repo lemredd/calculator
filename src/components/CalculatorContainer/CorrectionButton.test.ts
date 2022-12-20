bfc445cd5f48bd171d6254c48418cbb02b798ec3
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-20 18:24:17 +0800

    unit(component): ensure various clearing emissions

--- src/components/CalculatorContainer/CorrectionButton.test.ts
+++ src/components/CalculatorContainer/CorrectionButton.test.ts
@@ -1,20 +1,46 @@
 import { shallowMount } from "@vue/test-utils"
 
+import { Corrections } from "@/types/buttons"
+
 import Component from "./CorrectionButton.vue"
 
 describe("Component: CalculatorContainer/CorrectionButton", () => {
-	it("can emit custom value", async() => {
+	it("can emit clearing of user entry screen", async() => {
+		const props = {
+			"value": "CE" as Corrections
+		}
 		const wrapper = shallowMount(Component, {
-			"props": {
-				"value": "CE"
-			}
+			props
 		})
 		const correctionBtn = wrapper.find("button.correction-button")
-		const correctionBtnValue = wrapper.props("value")
-		expect(correctionBtn.text()).toEqual(String(correctionBtnValue))
 
 		await correctionBtn.trigger("click")
-		const expectedEmission = wrapper.emitted("appendToScreen")
-		expect(expectedEmission).toHaveProperty("0.0", correctionBtnValue)
+		expect(wrapper.emitted()).toHaveProperty(("clearEntryScreen"))
+	})
+
+	it("can emit clearing of all screens", async() => {
+		const props = {
+			"value": "C" as Corrections
+		}
+		const wrapper = shallowMount(Component, {
+			props
+		})
+		const correctionBtn = wrapper.find("button.correction-button")
+
+		await correctionBtn.trigger("click")
+		expect(wrapper.emitted()).toHaveProperty(("clearAllScreens"))
+	})
+
+	it("can emit clearing of one digit", async() => {
+		const props = {
+			"value": "" as Corrections
+		}
+		const wrapper = shallowMount(Component, {
+			props
+		})
+		const correctionBtn = wrapper.find("button.correction-button")
+
+		await correctionBtn.trigger("click")
+		expect(wrapper.emitted()).toHaveProperty(("clearOneDigit"))
 	})
 })
