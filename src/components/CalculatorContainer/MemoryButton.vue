177938a4130c5155e1f3111fa2e127eb6c43ffa3
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-20 16:09:26 +0800

    feat(component): make `MemoryButton`

---
+++ src/components/CalculatorContainer/MemoryButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type MemoryOperations = "MC"|"MR"|"M+"|"M-"|"MS"|"M"
+interface Props {
+	value: MemoryOperations
+}
+const props = defineProps<Props>()
+
+interface CustomEvents {
+	(event: "appendToScreen", valueToAppend: string): void
+}
+const emit = defineEmits<CustomEvents>()
+
+function appendToScreen() {
+	emit("appendToScreen", props.value)
+}
+</script>
+
+<template>
+	<button class="memory-button" @click="appendToScreen">
+		{{ props.value }}
+	</button>
+</template>
+
+<style scoped lang="scss">
+</style>
