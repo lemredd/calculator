f9eacd9a960fc41760a67821ec63651e7b548a10
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-20 15:46:04 +0800

    feat(component): make `EvaluationButton`
    this will also mutate another separate screen.
    The separate screen will be developed soon.
    
    fix lemredd/calculator#33

---
+++ src/components/CalculatorContainer/EvaluationButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type Evaluations = "="|"√"|"x²"|"1/x"|"%"
+interface Props {
+	value: Evaluations
+}
+const props = defineProps<Props>()
+
+interface CustomEvents {
+	(event: "appendToScreen", valueToAppend: string): void
+}
+const emit = defineEmits<CustomEvents>()
+
+function appendToScreen() {
+	emit("appendToScreen", props.value)
+}
+</script>
+
+<template>
+	<button class="evaluation-button" @click="appendToScreen">
+		{{ props.value }}
+	</button>
+</template>
+
+<style scoped lang="scss">
+</style>
