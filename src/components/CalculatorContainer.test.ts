74562dbe172aaa6f400efb58a646c99391c5d4ab
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-22 23:55:59 +0800

    test(intg): ensure proper selector & element

--- src/components/CalculatorContainer.test.ts
+++ src/components/CalculatorContainer.test.ts
@@ -5,7 +5,7 @@ import Component from "./CalculatorContainer.vue"
 describe("Component: CalculatorContainer", () => {
 	it("can evaluate a proper expression", async() => {
 		const wrapper = mount(Component)
-		const evaluationScrn = wrapper.find(".evaluation-screen").element as HTMLInputElement
+		const evaluationScrn = wrapper.find(".evaluation-screen")
 		const entryScrn = wrapper.find(".entry-screen").element as HTMLInputElement
 
 		// Find the digit "1" button and click it
@@ -13,13 +13,13 @@ describe("Component: CalculatorContainer", () => {
 		const [digital1Btn] = digitalBtns.filter(btn => btn.text() === "1")
 		await digital1Btn.trigger("click")
 		expect(entryScrn.value).toEqual("1")
-		expect(evaluationScrn.value).toEqual("")
+		expect(evaluationScrn.text()).toEqual("")
 
 		// Find the Addition button and click it
 		const operationalBtns = wrapper.findAll(".operational-button")
 		const [additionBtn] =  operationalBtns.filter(btn => btn.text() === "+")
 		await additionBtn.trigger("click")
-		expect(evaluationScrn.value).toEqual("1 +")
+		expect(evaluationScrn.text()).toEqual("1 +")
 
 		// click digit "1" button again
 		await digital1Btn.trigger("click")
@@ -29,7 +29,7 @@ describe("Component: CalculatorContainer", () => {
 		const evaluationBtns = wrapper.findAll(".evaluation-button")
 		const [equalBtn] =  evaluationBtns.filter(btn => btn.text() === "=")
 		await equalBtn.trigger("click")
-		expect(evaluationScrn.value).toEqual("1 + 1 =")
+		expect(evaluationScrn.text()).toEqual("1 + 1 =")
 		expect(entryScrn.value).toEqual("2")
 	})
 
