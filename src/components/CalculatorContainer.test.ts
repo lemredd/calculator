cb99f3971fbb58a7cd5033d1c7851e01f4795e0e
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-23 16:18:25 +0800

    test(intg): ensure percentage calculation

--- src/components/CalculatorContainer.test.ts
+++ src/components/CalculatorContainer.test.ts
@@ -33,5 +33,40 @@ describe("Component: CalculatorContainer", () => {
 		expect(entryScrn.value).toEqual("2")
 	})
 
+	it("can derive percentage as such: `entryValue * (previousEvaluatedValue / 100)`", async() => {
+		const wrapper = mount(Component)
+		const evaluationScrn = wrapper.find(".evaluation-screen")
+		const entryScrn = wrapper.find(".entry-screen").element as HTMLInputElement
+
+		// Find the digits "9" and "0" buttons and click them
+		const digitalBtns = wrapper.findAll(".digital-button")
+		const [digital9Btn, digital0Btn] = digitalBtns.filter(
+			btn => btn.text() === "9" || btn.text() === "0"
+		)
+		await digital9Btn.trigger("click")
+		expect(entryScrn.value).toEqual("9")
+		await digital0Btn.trigger("click")
+		expect(entryScrn.value).toEqual("90")
+		expect(evaluationScrn.text()).toEqual("")
+
+		// Find the Percentage button and click it
+		const evaluationBtns = wrapper.findAll(".evaluation-button")
+		const [percentageBtn] = evaluationBtns.filter(btn => btn.text() === "%")
+		await percentageBtn.trigger("click")
+
+		// This implicitly expects that `previousEvaluatedValue === 0`
+		expect(entryScrn.value).toEqual("0")
+		expect(evaluationScrn.text()).toEqual("0")
+
+		// Mock mutation of `previousEvaluatedValue`
+		const wrapperInternals = wrapper.vm as any
+		wrapperInternals.previousEvaluatedValue = "50"
+		await digital9Btn.trigger("click")
+		await digital0Btn.trigger("click")
+		await percentageBtn.trigger("click")
+		expect(entryScrn.value).toEqual("45")
+		expect(evaluationScrn.text()).toEqual("45")
+	})
+
 	it.todo("can continuously evaluate")
 })
