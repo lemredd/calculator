72439b122966608db0ddfe1f018b9ef9c4b4b77b
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2022-12-27 18:58:37 +0800

    test(component): ensure sign altering emission

--- src/components/CalculatorContainer/DigitalButton.test.ts
+++ src/components/CalculatorContainer/DigitalButton.test.ts
@@ -17,4 +17,19 @@ describe("Component: CalculatorContainer/DigitalButton", () => {
 		const expectedEmission = wrapper.emitted("appendToScreen")
 		expect(expectedEmission).toHaveProperty("0.0", digitalBtnValue)
 	})
+
+	it("can emit altering of sign", async() => {
+		const wrapper = shallowMount(Component, {
+			"props": {
+				"value": "+/-"
+			}
+		})
+		const digitalBtn = wrapper.find("button.digital-button")
+		const digitalBtnValue = wrapper.props("value")
+		expect(digitalBtn.text()).toEqual(String(digitalBtnValue))
+
+		await digitalBtn.trigger("click")
+		const expectedEmission = wrapper.emitted("alterSign")
+		expect(expectedEmission).toBeTruthy()
+	})
 })
