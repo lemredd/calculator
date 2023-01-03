96112d38db7a15ab224fdb0949b070a5951de202
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-03 23:34:12 +0800

    chore(ui): give distinct class
    if `valueToDisplay === ""`

--- src/components/CalculatorContainer/EntryScreen.vue
+++ src/components/CalculatorContainer/EntryScreen.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
+import { computed } from "vue"
+
 interface Props {
 	valueToDisplay: string
 }
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const entryScreenClasses = computed(() => ({
+	"empty": props.valueToDisplay === ""
+}))
 </script>
 
 <template>
-	<span class="entry-screen">
+	<span class="entry-screen" :class="entryScreenClasses">
 		{{ valueToDisplay }}
 	</span>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.entry-screen.empty {
+	@apply h-10;
+}
+</style>
