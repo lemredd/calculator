a9f10d9e17ca175ce135ca11dc1171f214ed4614
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-04 14:43:19 +0800

    test(component): ensure evaluation result emission

--- src/components/CalculatorContainer/EvaluationButton.test.ts
+++ src/components/CalculatorContainer/EvaluationButton.test.ts
@@ -1,11 +1,15 @@
 import { shallowMount } from "@vue/test-utils"
 
+import evaluate from "@/CalculatorContainer/helpers/evaluate"
+
 import Component from "./EvaluationButton.vue"
 
 describe("Component: CalculatorContainer/EvaluationButton", () => {
 	it("can emit custom value", async() => {
+		const expressionToEvaluate = "1+1"
 		const wrapper = shallowMount(Component, {
 			"props": {
+				expressionToEvaluate,
 				"value": "="
 			}
 		})
@@ -16,5 +20,21 @@ describe("Component: CalculatorContainer/EvaluationButton", () => {
 		await evaluationBtn.trigger("click")
 		const expectedEmission = wrapper.emitted("appendToScreen")
 		expect(expectedEmission).toHaveProperty("0.0", evaluationBtnValue)
+	})
+
+	it("can emit evaluation result based on expression", async() => {
+		const expressionToEvaluate = "1+1"
+		const wrapper = shallowMount(Component, {
+			"props": {
+				expressionToEvaluate,
+				"value": "="
+			}
+		})
+		const evaluationBtn = wrapper.find(".evaluation-button")
+		await evaluationBtn.trigger("click")
+
+		const expectedEvaluationResult = evaluate(expressionToEvaluate)
+		const expectedEmission = wrapper.emitted("emitEvaluationResult")
+		expect(expectedEmission).toHaveProperty("0.0", expectedEvaluationResult)
 	})
 })
