ade4c481e4ee1a0987f6342d72c40626d808de58
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-05 20:59:27 +0800

    test(component): ensure continuous evaluation
    with `=` button

--- src/components/CalculatorContainer/EvaluationButton.test.ts
+++ src/components/CalculatorContainer/EvaluationButton.test.ts
@@ -22,4 +22,41 @@ describe("Component: CalculatorContainer/EvaluationButton", () => {
 		expect(expectedEmission).toHaveProperty("0.0", evaluationBtnValue)
 		expect(expectedEmission).toHaveProperty("0.1", expectedEvaluationResult)
 	})
+
+	it("can evaluate continuously with `=` button", async() => {
+		const expressionToEvaluate = "1+1"
+		const expressionAndPreviousResultInformation = {
+			"hasSavedPreviousResult": false,
+			"operation": "+",
+			"previousResult": "0",
+			"rightEntry": null
+		}
+		const wrapper = shallowMount(Component, {
+			"props": {
+				expressionAndPreviousResultInformation,
+				expressionToEvaluate,
+				"operation": "+",
+				"value": "="
+			}
+		})
+		const evaluationBtn = wrapper.find(".evaluation-button")
+		await evaluationBtn.trigger("click")
+
+		const updatedExpressionToEvaluate = "2+1"
+		const updatedexpressionAndPreviousResultInformation = {
+			"hasSavedPreviousResult": true,
+			"operation": "+",
+			"previousResult": 2,
+			"rightEntry": 1
+		}
+		await wrapper.setProps({
+			"expressionAndPreviousResultInformation": updatedexpressionAndPreviousResultInformation,
+			"expressionToEvaluate": updatedExpressionToEvaluate
+		})
+		await evaluationBtn.trigger("click")
+
+		const expectedEmission = wrapper.emitted("emitEvaluationResult")
+		const expectedEvaluationResult = evaluate(updatedExpressionToEvaluate)
+		expect(expectedEmission).toHaveProperty("1.1", expectedEvaluationResult)
+	})
 })
