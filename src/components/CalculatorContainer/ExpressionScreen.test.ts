ba9d2ce1217d10d2ac7107338fbde36919ef70f0
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-07 06:54:28 +0800

    test(component): ensure expression to display

--- src/components/CalculatorContainer/ExpressionScreen.test.ts
+++ src/components/CalculatorContainer/ExpressionScreen.test.ts
@@ -3,26 +3,38 @@ import { shallowMount } from "@vue/test-utils"
 import Component from "./ExpressionScreen.vue"
 
 describe("Component: CalculatorContainer/ExpressionScreen", () => {
-	it("can change displayed value", async() => {
+	it("can determine expression to display", async() => {
 		const wrapper = shallowMount(Component, {
 			"props": {
-				"valueToDisplay": "0"
+				"entry": "",
+				"evaluation": null,
+				"hasPreviousEntry": true,
+				"operation": "+",
+				"previousEntry": 1,
+				"previousExpressionEvaluated": ""
 			}
 		})
 		const calculatorScreen = wrapper.find(".expression-screen")
-		const calculatorScreenInitialValue = wrapper.props("valueToDisplay")
-		expect(calculatorScreen.text()).toEqual(calculatorScreenInitialValue)
+		const expectedInitialTextValue = "1 +"
+		expect(calculatorScreen.text()).toEqual(expectedInitialTextValue)
 
 		await wrapper.setProps({
-			"valueToDisplay": "1"
+			"evaluation": "=",
+			"previousExpressionEvaluated": "1+1"
 		})
-		expect(calculatorScreen.text()).toEqual("1")
+		const expectedFinalTextValue = "1 + 1 ="
+		expect(calculatorScreen.text()).toEqual(expectedFinalTextValue)
 	})
 
 	it("must have distinct class if `valueToDisplay` is empty", () => {
 		const wrapper = shallowMount(Component, {
 			"props": {
-				"valueToDisplay": ""
+				"entry": "",
+				"evaluation": null,
+				"hasPreviousEntry": false,
+				"operation": null,
+				"previousEntry": null,
+				"previousExpressionEvaluated": ""
 			}
 		})
 
