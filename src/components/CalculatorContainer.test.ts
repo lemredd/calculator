8a95c09a3c548e6f53a7e6fea3af4b7cd5c3b3ba
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-10 13:58:48 +0800

    test(intg): ensure adding to history list
    `historyItems` are added for every `=` evaluation

--- src/components/CalculatorContainer.test.ts
+++ src/components/CalculatorContainer.test.ts
@@ -366,4 +366,33 @@ describe("Component: CalculatorContainer", () => {
 		await decimalBtn.trigger("click")
 		expect(entryScrn.text()).toEqual("25.")
 	})
+	it("can add to history list", async() => {
+		const wrapper = mount(Component)
+
+		// Find the digit "1" button and click it
+		const digitalBtns = wrapper.findAll(".digital-button")
+		const [digital1Btn] = digitalBtns.filter(btn => btn.text() === "1")
+		await digital1Btn.trigger("click")
+
+		// Find the Addition button and click it
+		const operationalBtns = wrapper.findAll(".operational-button")
+		const [additionBtn] =  operationalBtns.filter(btn => btn.text() === "+")
+		await additionBtn.trigger("click")
+
+		// click digit "1" button again
+		await digital1Btn.trigger("click")
+
+		// Find the Equal button and click it
+		const evaluationBtns = wrapper.findAll(".evaluation-button")
+		const [equalBtn] =  evaluationBtns.filter(btn => btn.text() === "=")
+		await equalBtn.trigger("click")
+
+		const wrapperInternals = wrapper.vm as any
+		const expectedHistoryItem = {
+			"leftOperand": 1,
+			"operation": "+",
+			"rightOperand": 1
+		}
+		expect(wrapperInternals.historyList).toContainEqual(expectedHistoryItem)
+	})
 })
