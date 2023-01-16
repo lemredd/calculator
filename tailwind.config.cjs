e3ba52191d3671c559ca2753a5af19de47b5c2bc
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-16 21:31:16 +0800

    chore(ui): add color schemes with custom names
    Color scheme is based from https://colorkit.co/palette/4e9e94-f7cb45-f096e3-8a94f8-2e66d2/

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
 		"./src/**/.{vue,js,ts}"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"calculator-candy": "#f096e3",
+				"calculator-mint": "#4e9e94",
+				"calculator-mustard": "#f7cb45",
+				"calculator-ocean": "#2e66d2",
+				"calculator-sky": "#8a94f8",
+			}
+		}
 	},
 	plugins: [],
 }
