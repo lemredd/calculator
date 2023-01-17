3564bef973dc828a797134ba4f8c82ea432174b3
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-17 11:46:16 +0800

    test(component): ensure reverse listed items

--- src/components/CalculatorContainer/HistoryContainer.test.ts
+++ src/components/CalculatorContainer/HistoryContainer.test.ts
@@ -42,4 +42,40 @@ describe("Component: CalculatorContainer/HistoryContainer", () => {
 		const expectedEmission = wrapper.emitted("revertToChosenHistory")
 		expect(expectedEmission).toHaveProperty("0.0", historyItem)
 	})
+
+	it("should display items in reverse", async() => {
+		const historyItem1 = {
+			"leftOperand": 1,
+			"operation": "+" as Operations,
+			"rightOperand": 1
+		}
+		const historyItem2 = {
+			"leftOperand": 2,
+			"operation": "+" as Operations,
+			"rightOperand": 2
+		}
+		const historyItem3 = {
+			"leftOperand": 3,
+			"operation": "+" as Operations,
+			"rightOperand": 3
+		}
+		const historyList = [historyItem1, historyItem2, historyItem3]
+		const props = { historyList }
+		const wrapper = shallowMount(Component,{ props })
+
+		// mock `isShowingHistoryList = true`
+		const wrapperInternals = wrapper.vm as any
+		wrapperInternals.isShowingHistoryList = true
+		await nextTick()
+
+		const historyListHiddenByDefault = wrapper.find(".history-list.hidden-by-default")
+		const [
+			historyListItem1,
+			historyListItem2,
+			historyListItem3
+		] = historyListHiddenByDefault.findAllComponents({ "name": "HistoryListItem" })
+		expect(historyListItem1.props("historyItem")).toStrictEqual(historyItem3)
+		expect(historyListItem2.props("historyItem")).toStrictEqual(historyItem2)
+		expect(historyListItem3.props("historyItem")).toStrictEqual(historyItem1)
+	})
 })
