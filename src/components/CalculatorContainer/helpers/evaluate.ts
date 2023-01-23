2cb1aa12007d375e70db2c44f68d28743827bc27
Jarlem Red J. de Peralta <lmoa.jhdp@gmail.com>
2023-01-23 17:32:29 +0800

    fix(helpers): ensure proper throwing of message

--- src/components/CalculatorContainer/helpers/evaluate.ts
+++ src/components/CalculatorContainer/helpers/evaluate.ts
@@ -1,10 +1,19 @@
 import evaluator from "math-expression-evaluator"
 
+interface ExpressionError {
+	message: string
+}
+
 export default function(expression: string) {
 	let normalizedExpression = expression
 
 	if(normalizedExpression.includes("×")) normalizedExpression = normalizedExpression.replace("×", "*")
 	if(normalizedExpression.includes("÷")) normalizedExpression = normalizedExpression.replace("÷", "/")
 
-	return evaluator.eval(normalizedExpression)
+	try {
+		return evaluator.eval(normalizedExpression)
+	} catch (error: unknown) {
+		const expressionError = error as ExpressionError
+		throw `${expressionError.message} (expression: ${normalizedExpression})`
+	}
 }
